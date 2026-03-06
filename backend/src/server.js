a5c4b90faf75a19345e4d6036862114d714ce53f
mastergogy-ui <mastergogy@gmail.com>
2026-03-06 13:40:12 -0800

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,6 @@
-import dotenv from 'dotenv';
+const express = require("express");
+import dotenv from 'dotenv'; 
+
 import app from './app.js';
 import { connectDB } from './config/db.js';
 
@@ -7,4 +9,5 @@ const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  const adRoutes = require("./routes/adRoutes");
 });
