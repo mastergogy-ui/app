c3a333cef1e06ae1a1611575ac9b16c08e2ac4d6
mastergogy-ui <mastergogy@gmail.com>
2026-03-08 11:29:56 -0700

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,71 +7,79 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/* HTTP SERVER */
-
 const server = http.createServer(app);
-
-/* SOCKET.IO */
 
 const { Server } = require("socket.io");
 
 const io = new Server(server,{
-  cors:{
-    origin:"*"
-  }
+cors:{
+origin:"*"
+}
 });
-
-/* CHAT STORAGE (temporary memory) */
 
 let chats = {};
 
-/* SOCKET CONNECTION */
-
 io.on("connection",(socket)=>{
 
-  console.log("User connected");
+console.log("User connected");
 
-  socket.on("joinRoom",(adId)=>{
+socket.on("joinRoom",(adId)=>{
 
-    socket.join(adId);
-
-  });
-
-  socket.on("sendMessage",({adId,message})=>{
-
-    if(!chats[adId]){
-
-      chats[adId] = [];
-
-    }
-
-    const msg = {
-      message,
-      time:Date.now()
-    };
-
-    chats[adId].push(msg);
-
-    io.to(adId).emit("receiveMessage",msg);
-
-  });
+```
+socket.join(adId);
+```
 
 });
 
-/* API FOR OLD MESSAGES */
+socket.on("sendMessage",({adId,message})=>{
+
+```
+if(!chats[adId]){
+
+  chats[adId] = [];
+
+}
+
+const msg = {
+  message,
+  time:Date.now()
+};
+
+chats[adId].push(msg);
+
+io.to(adId).emit("receiveMessage",msg);
+```
+
+});
+
+socket.on("typing",(adId)=>{
+
+```
+socket.to(adId).emit("typing");
+```
+
+});
+
+socket.on("messageSeen",(adId)=>{
+
+```
+socket.to(adId).emit("seen");
+```
+
+});
+
+});
 
 app.get("/chat/:adId",(req,res)=>{
 
-  const { adId } = req.params;
+const { adId } = req.params;
 
-  res.json(chats[adId] || []);
+res.json(chats[adId] || []);
 
 });
 
-/* START SERVER */
-
 server.listen(5000,()=>{
 
-  console.log("Server running");
+console.log("Server running on 5000");
 
 });
