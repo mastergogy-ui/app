7db54700ec25e7edaa562c573c51a8d3688de9e3
mastergogy-ui <mastergogy@gmail.com>
2026-03-08 11:09:48 -0700

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,77 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import adRoutes from "./routes/adRoutes.js";
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-/* TEST ROOT */
-app.get("/", (req, res) => {
-  res.send("Backend running");
+/* HTTP SERVER */
+
+const server = http.createServer(app);
+
+/* SOCKET.IO */
+
+const { Server } = require("socket.io");
+
+const io = new Server(server,{
+  cors:{
+    origin:"*"
+  }
 });
 
-/* DATABASE */
-mongoose.connect(process.env.MONGO_URI || "")
-.then(()=>console.log("MongoDB connected"))
-.catch((err)=>console.log("MongoDB error:", err));
+/* CHAT STORAGE (temporary memory) */
 
-/* ADS ROUTES */
-app.use("/api/ads", adRoutes);
+let chats = {};
 
-const PORT = process.env.PORT || 10000;
+/* SOCKET CONNECTION */
 
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+io.on("connection",(socket)=>{
+
+  console.log("User connected");
+
+  socket.on("joinRoom",(adId)=>{
+
+    socket.join(adId);
+
+  });
+
+  socket.on("sendMessage",({adId,message})=>{
+
+    if(!chats[adId]){
+
+      chats[adId] = [];
+
+    }
+
+    const msg = {
+      message,
+      time:Date.now()
+    };
+
+    chats[adId].push(msg);
+
+    io.to(adId).emit("receiveMessage",msg);
+
+  });
+
+});
+
+/* API FOR OLD MESSAGES */
+
+app.get("/chat/:adId",(req,res)=>{
+
+  const { adId } = req.params;
+
+  res.json(chats[adId] || []);
+
+});
+
+/* START SERVER */
+
+server.listen(5000,()=>{
+
+  console.log("Server running");
+
 });
