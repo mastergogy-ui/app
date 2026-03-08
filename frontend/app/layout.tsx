0f93c806a80bad87b7c70e72628abf3d0f1dcb2d
mastergogy-ui <mastergogy@gmail.com>
2026-03-08 13:17:23 -0700

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,26 +1,23 @@
 import "./globals.css";
 import Navbar from "../components/Navbar";
+import FloatingPostAd from "../components/FloatingPostAd";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
   return (
-
     <html lang="en">
-
       <body>
 
         <Navbar />
 
         {children}
 
+        <FloatingPostAd />
+
       </body>
-
     </html>
-
   );
-
 }
