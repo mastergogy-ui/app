ca32b250d6745eecda82b3018db9b042c71086a4
mastergogy-ui <mastergogy@gmail.com>
2026-03-08 13:19:44 -0700

    Update api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,5 +1,32 @@
 import axios from "axios";
 
+const BASE_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL ||
+  "https://mahalakshmi.onrender.com";
+
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+export const get = async (url: string) => {
+  const res = await api.get(url);
+  return res.data;
+};
+
+export const post = async (url: string, data: any) => {
+  const res = await api.post(url, data);
+  return res.data;
+};
+
+export const put = async (url: string, data: any) => {
+  const res = await api.put(url, data);
+  return res.data;
+};
+
+export const del = async (url: string) => {
+  const res = await api.delete(url);
+  return res.data;
+};
