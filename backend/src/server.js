9e51a738e7d94cd3eb038ff0f6451eac8a04f64b
mastergogy-ui <mastergogy@gmail.com>
2026-03-09 03:05:34 -0700

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
@@ -31,5 +31,5 @@ app.get("/api/ads", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
