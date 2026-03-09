f1343e0ef126d860d03f4e6af136b1c725561333
mastergogy-ui <mastergogy@gmail.com>
2026-03-08 23:31:51 -0700

    Update tailwind.config.ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,7 +1,11 @@
-import type { Config } from 'tailwindcss';
-
-export default {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
-  plugins: []
-} satisfies Config;
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
