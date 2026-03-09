f633ae8500c60f3f8d9493c95113981faae5b0ac
mastergogy-ui <mastergogy@gmail.com>
2026-03-08 23:34:25 -0700

    Update api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,12 +1,12 @@
-import axios from "axios";
+import axios from "axios"
 
 const BASE_URL =
   process.env.NEXT_PUBLIC_BACKEND_URL ||
-  "https://mahalakshmi.onrender.com";
+  "https://mahalakshmi.onrender.com"
 
 export const api = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json"
   }
-});
+})
