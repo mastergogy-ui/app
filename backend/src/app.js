2f35806d85bf2ba45b16dfc7b6adddea9bb57192
mastergogy-ui <mastergogy@gmail.com>
2026-03-09 04:21:58 -0700

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-
+import adRoutes from "./routes/adRoutes.js";
 const app = express();
-
+app.use("/api/ads",adRoutes);
 app.use(cors({
 origin: "*",
 methods: ["GET","POST","PUT","DELETE"],
