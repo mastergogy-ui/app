3f6c752560d5403101135fe655ad17cc696b6115
mastergogy-ui <mastergogy@gmail.com>
2026-03-10 07:09:43 -0700

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import { adminLogin, login, register } from '../controllers/authController.js';
+import express from "express";
+import { register, login, adminLogin } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/admin/login', adminLogin);
+router.post("/register", register);
+router.post("/login", login);
+router.post("/admin-login", adminLogin);
 
 export default router;
