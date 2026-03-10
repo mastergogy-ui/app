7ef492c1fa84e6ecc2d49367b79ff7a29c8d5894
mastergogy-ui <mastergogy@gmail.com>
2026-03-10 14:49:30 -0700

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,17 +1,17 @@
 import express from "express";
-import { register, login, adminLogin } from "../controllers/authController.js";
+import { register, login, getMe, updateProfile } from "../controllers/authController.js";
 import { googleLogin } from "../controllers/googleAuthController.js";
+import { protectUser } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-/* USER AUTH */
-
+/* PUBLIC ROUTES */
 router.post("/register", register);
 router.post("/login", login);
-router.post("/admin-login", adminLogin);
-
-/* GOOGLE LOGIN */
-
 router.post("/google", googleLogin);
+
+/* PROTECTED ROUTES */
+router.get("/me", protectUser, getMe);
+router.put("/profile", protectUser, updateProfile);
 
 export default router;
