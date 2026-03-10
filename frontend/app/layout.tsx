0053ae6ad55d3e777813fe0c2a56dbabeb4d5b5d
mastergogy-ui <mastergogy@gmail.com>
2026-03-10 09:35:15 -0700

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import "./globals.css"
 import { GoogleOAuthProvider } from "@react-oauth/google"
+import React from "react"
 
 export default function RootLayout({
   children,
@@ -11,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+        <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string}>
           {children}
         </GoogleOAuthProvider>
       </body>
