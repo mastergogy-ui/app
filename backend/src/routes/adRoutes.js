36ab710204e7e3c434f4937fb6b83911fd6eb7d4
mastergogy-ui <mastergogy@gmail.com>
2026-03-10 14:47:38 -0700

    Update adRoutes.js

--- backend/src/routes/adRoutes.js
+++ backend/src/routes/adRoutes.js
@@ -1,16 +1,32 @@
 import express from "express";
-import { createAd, getAds } from "../controllers/adController.js";
+import { 
+  createAd, 
+  getAds, 
+  getAdById,
+  updateAd, 
+  deleteAd,
+  getUserAds,
+  toggleSaveAd,
+  getSavedAds,
+  getCategories
+} from "../controllers/adController.js";
+import { protectUser } from "../middlewares/auth.js";
 import multer from "multer";
 
 const router = express.Router();
-
-/* TEMP STORAGE FOR CLOUDINARY */
 const upload = multer({ dest: "temp/" });
 
-/* CREATE AD */
-router.post("/", upload.single("image"), createAd);
-
-/* GET ALL ADS */
+/* PUBLIC ROUTES */
 router.get("/", getAds);
+router.get("/categories", getCategories);
+router.get("/:id", getAdById);
+router.get("/user/:userId", getUserAds);
+
+/* PROTECTED ROUTES */
+router.post("/", protectUser, upload.array("images", 8), createAd);
+router.put("/:id", protectUser, upload.array("images", 8), updateAd);
+router.delete("/:id", protectUser, deleteAd);
+router.post("/:id/save", protectUser, toggleSaveAd);
+router.get("/saved/me", protectUser, getSavedAds);
 
 export default router;
