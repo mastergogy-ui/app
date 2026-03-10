e9fc3336405039119922888748ef0012d25b7ef3
mastergogy-ui <mastergogy@gmail.com>
2026-03-10 14:42:34 -0700

    Create Message.js

---
+++ backend/src/models/Message.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const MessageSchema = new mongoose.Schema(
+{
+  conversation: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation', required: true },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  text: { type: String, required: true },
+  readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  isDeleted: { type: Boolean, default: false }
+},
+{ timestamps: true }
+);
+
+MessageSchema.index({ conversation: 1, createdAt: -1 });
+
+export default mongoose.model("Message", MessageSchema);
