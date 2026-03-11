5f6c08432130cd3491f312852aafe68b967a8d30
mastergogy-ui <mastergogy@gmail.com>
2026-03-11 02:52:46 -0700

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  images: {
-    unoptimized: true,
-  },
+  reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['res.cloudinary.com', 'lh3.googleusercontent.com'],
+  },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
