6282eee9e4fdafe2199444b827e7915795252ca0
mastergogy-ui <mastergogy@gmail.com>
2026-03-11 00:40:51 -0700

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#002f34',
-        secondary: '#23e5db',
-        accent: '#ffce32',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
+export default config
