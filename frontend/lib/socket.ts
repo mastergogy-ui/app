7430bed8df95497debea8a7b99492c2fc851b424
mastergogy-ui <mastergogy@gmail.com>
2026-03-15 00:44:02 -0700

    Update socket.ts

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -2,8 +2,10 @@ import { io } from "socket.io-client"
 
 const URL =
   process.env.NEXT_PUBLIC_BACKEND_URL ||
+  "https://app-production-7a53.up.railway.app" ||
   "https://mahalakshmi.onrender.com"
 
-export const socket = io(URL,{
-  transports:["websocket"]
+export const socket = io(URL, {
+  transports: ["websocket"],
+  withCredentials: true
 })
